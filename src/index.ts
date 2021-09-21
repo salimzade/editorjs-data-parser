3a32cc864fa172738e8c3aef3ea672be0b23ab4c
salimzade <salymzade@gmail.com>
2021-09-21 18:33:33 +0300

    Error Fix (can't read foreach property of undefined)

--- src/index.ts
+++ src/index.ts
@@ -13,52 +13,57 @@ import {
     warning,
 } from './tools';
 
-const editorJsParser = (value: { array: any[] }) => {
+const editorJsParser = (value: any[]) => {
     let editorData = '';
 
-    value.array.forEach((element) => {
-        switch (element.type) {
-            case 'checklist':
-                editorData += checklist(element.data.items);
-                break;
-            case 'code':
-                editorData += code(element);
-            case 'embed':
-                editorData += embed(element.data);
-                break;
-            case 'header':
-                editorData += header(element.data.text, element.data.level);
-                break;
-            case 'image':
-                editorData += image(element.data);
-                break;
-            case 'link':
-                editorData += link(element.data.link);
-                break;
-            case 'list':
-                editorData += list(element.data.items);
-                break;
-            case 'paragraph':
-                editorData += paragraph(element.data.text);
-                break;
-            case 'quote':
-                editorData += quote(element.data.caption, element.data.text);
-                break;
-            case 'raw':
-                editorData += raw(element.data.html);
-                break;
-            case 'table':
-                editorData += table(element);
-                break;
-            case 'warning':
-                editorData += warning(element.data);
-                break;
-            default:
-                editorData += '';
-        }
-    });
+    try {
+        value.forEach((element: { type?: string; data: any; }) => {
+            switch (element.type) {
+                case 'checklist':
+                    editorData += checklist(element.data.items);
+                    break;
+                case 'code':
+                    editorData += code(element);
+                    break;
+                case 'embed':
+                    editorData += embed(element.data);
+                    break;
+                case 'header':
+                    editorData += header(element.data.text, element.data.level);
+                    break;
+                case 'image':
+                    editorData += image(element.data);
+                    break;
+                case 'link':
+                    editorData += link(element.data.link);
+                    break;
+                case 'list':
+                    editorData += list(element.data.items);
+                    break;
+                case 'paragraph':
+                    editorData += paragraph(element.data.text);
+                    break;
+                case 'quote':
+                    editorData += quote(element.data.caption, element.data.text);
+                    break;
+                case 'raw':
+                    editorData += raw(element.data.html);
+                    break;
+                case 'table':
+                    editorData += table(element);
+                    break;
+                case 'warning':
+                    editorData += warning(element.data);
+                    break;
+                default:
+                    editorData += '';
+            }
+        });
+    } catch(error){
+        console.error(error);
+    }
 
     return editorData;
 };
 
-module.exports = editorJsParser
+exports.editorJsParser = editorJsParser;
