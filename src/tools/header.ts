a4fdab4212b90830ce1579838d02a93941999b7d
salimzade <salymzade@gmail.com>
2021-09-21 18:37:02 +0300

    Header block fix

--- src/tools/header.ts
+++ src/tools/header.ts
@@ -5,19 +5,19 @@ export const header = (param: string, type: number) => {
             headerValue += `<h1>${param}</h1>`;
             break;
         case 2:
-            headerValue += `<h2>${param}</h2a>`;
+            headerValue += `<h2>${param}</h2>`;
             break;
         case 3:
-            headerValue += `<h3>${param}</h3a>`;
+            headerValue += `<h3>${param}</h3>`;
             break;
         case 4:
-            headerValue += `<h4>${param}</h4a>`;
+            headerValue += `<h4>${param}</h4>`;
             break;
         case 5:
-            headerValue += `<h5>${param}</h5a>`;
+            headerValue += `<h5>${param}</h5>`;
             break;
         case 6:
-            headerValue += `<h6>${param}</h6a>`;
+            headerValue += `<h6>${param}</h6>`;
             break;
         default:
             headerValue += ''
