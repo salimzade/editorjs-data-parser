31fe1cd022ff62e7f3cf59621af3b2e7c77866ee
salimzade <salymzade@gmail.com>
2021-10-15 22:16:52 +0300

    fixed the problem with parsing the checklist

---
+++ src/tools/components/checkList.ts
@@ -0,0 +1,17 @@
+let value = '';
+export const checklist = (param: { checked: boolean; text: string }[]) => {
+  value = '';
+  param.forEach((items: { checked: boolean; text: string }) => {
+    value += `<div class="ejs-form-check">
+                    <input class="ejs-form-check__input" type="radio" name="ejsRadios" id="ejsRadio" value="option1" ${
+                      items.checked ? 'checked' : ''
+                    }>
+                    <label class="ejs-form-check__label" for="ejsRadio">
+                    ${items.text}
+                    </label>
+                </div>
+      `;
+  });
+
+  return value;
+};
