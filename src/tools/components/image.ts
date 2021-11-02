0267fb60f570e8dfd0737f8575e667d6204c8d6f
salimzade <salymzade@gmail.com>
2021-11-02 15:10:47 +0300

    added image sizing tool

--- src/tools/components/image.ts
+++ src/tools/components/image.ts
@@ -1,3 +1,17 @@
-export const image = (param: { file: { url: string }; caption: string }) => {
-  return `<img src='${param.file.url}' alt='${param.caption}'/>`;
+interface ImgTypes {
+  stretched: boolean;
+  withBorder: boolean;
+  withBackground: boolean;
+  file: {
+    url: string;
+  };
+  caption: string;
+}
+
+export const image = (param: ImgTypes) => {
+  const imgParams = `${param.stretched ? 'ejs-img__fullwidth' : ''} ${
+    param.withBorder ? 'ejs-image__withBorder' : ''
+  } ${param.withBackground ? 'ejs-img__background' : ''}`;
+
+  return `<div class="${imgParams}"><img class="ejs-image" src="${param.file.url}" alt="${param.caption}"></div>`;
 };
